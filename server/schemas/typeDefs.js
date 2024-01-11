d0b3843ec33f944a478eb4af0db9579c28989e23
Baljot Hansi <baljotshansi@gmail.com>
2024-01-11 09:34:19 -0400

    Defined typeDefs for models. Queries and mutations to come.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,3 +1,34 @@
-const typeDefs = ``;
+const typeDefs = `
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+    bookCount: Int
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+
+  }
+
+  type Mutation {
+
+  }
+`;
 
 module.exports = typeDefs;
